f136e1609c52f177164428a022da7c5cacf174a7
DenTerr <sasin.danila1047@gmail.com>
2025-01-04 20:16:55 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,24 @@
+my_dict = ({'Ivan' : 1998,
+    'Masha' : 2002,
+    'Alex' : 1970})
+
+
+print (my_dict)
+print (my_dict['Ivan'])
+print (my_dict.get ('Alla'))
+
+my_dict ['Dima'] = 2006
+my_dict ['Liza'] = 1999
+
+del my_dict ['Alex']
+     print (my_dict)
+     my_set = { 125, 395, 'string', 'home', 125, True, 'string'}
+     print(),print (my_set)
+
+my_set.add(886)
+my_set.add(750)
+my_set.add(125)
+
+my_set.remove('home')
+
+print(my_set)
