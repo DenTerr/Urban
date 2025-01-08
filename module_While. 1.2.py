b2624f8e0a289a38654f1ee8a54c67d723f39188
DenTerr <sasin.danila1047@gmail.com>
2025-01-08 22:08:14 +0300

    Add files via upload

---
+++ module_While. 1.2.py
@@ -0,0 +1,20 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+index = 0
+
+while index < len(my_list):
+
+    number = my_list[index]
+
+    if number < 0 :
+        break
+
+    if number == 0 :
+        index += 1
+        continue
+
+    if number >= 0 :
+        index += 1
+        print(number)
+        continue
+
